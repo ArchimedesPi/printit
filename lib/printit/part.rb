c02a33bf7040924962c07029ecdf6a2b4ca2211b
Liam M <archimedespi3141@gmail.com>
2014-05-18 16:00:21 -0500

    Added simple slice structure

--- lib/printit/part.rb
+++ lib/printit/part.rb
@@ -1,17 +1,21 @@
+require 'pathname'
+
 module PrintIt
   class Part
     attr_reader :name, :file, :multiplier, :tile
     attr_accessor :columns, :rows, :extruder
     def initialize(spec)
       @name = spec[:name]
-      @file = spec[:file]
+      @file = "./stls/#{spec[:file]}"
       @multiplier = spec[:multiplier] || 1
       @tile = spec[:tile] || false
     end
     
-    def slice(spec)
+    def slice(spec, outputfile=nil)
+      outputfile ||= "./gcode/#{Pathname.new(@file).basename}"
     end
-    
+
     def print(printer, options={})
+    end
   end
 end

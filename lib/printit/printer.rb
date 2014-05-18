d3c513e5ba6e12145a5d34bcd5d37b85adc5f5bd
Liam M <archimedespi3141@gmail.com>
2014-05-18 15:07:06 -0500

    Added backend to printer

--- lib/printit/printer.rb
+++ lib/printit/printer.rb
@@ -1,11 +1,15 @@
 module PrintIt
   class Printer
-    attr_reader :name, :port, :extruder
+    attr_reader :name, :type, :port, :extruder, :priority
+    attr_accessor :current_part, :running
     
     def initialize(spec)
       @name = spec[:name]
+      @type = spec[:type]
       @port = spec[:port]
       @extruder = spec[:extruder]
+      @priority = spec[:extruder] || 0
+      @running = spec[:running] || false
     end
   end
 end

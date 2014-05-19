f088c1a23cad88f6a136b0d63d76b10d8ece8cc1
Liam M <archimedespi3141@gmail.com>
2014-05-19 16:09:14 -0500

    Refactored version grabber

--- lib/printit/version.rb
+++ lib/printit/version.rb
@@ -2,8 +2,11 @@ module PrintIt
   VERSION = "0.0.1"
   module_function
   def slic3r_version
-    lines = []
-    IO.popen(['slic3r', '--version']).each {|l| lines << l }
-    lines.join
+    ver = ''
+    slicer = IO.popen(['slic3r', '--version'])
+    slicer.each {|l| ver << l}
+    Process.kill("TERM", slicer.pid)
+    slicer.close
+    ver
   end
 end

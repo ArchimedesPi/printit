9d91f5deef677b36386b0ded262eff9edbd2ab0e
Liam M <archimedespi3141@gmail.com>
2014-05-19 10:12:13 -0500

    Part slicer rewritten to facilitate print system

--- lib/printit/part.rb
+++ lib/printit/part.rb
@@ -2,21 +2,20 @@ require 'pathname'
 
 module PrintIt
   class Part
-    attr_reader :name, :file, :multiplier, :tile
+    attr_reader :name, :file, :multiplier, :tile, :slicedfile
     attr_accessor :columns, :rows, :extruder
     def initialize(spec)
       @name = spec[:name]
       @file = "./stls/#{spec[:file]}"
       @multiplier = spec[:multiplier] || 1
       @tile = spec[:tile] || false
+      @silcedfile = "./gcode/#{Pathname.new(@file).basename}"
     end
     
-    def slice(config=nil, outputfile=nil)
-      outputfile ||= "./gcode/#{Pathname.new(@file).basename}"
-      IO.popen("slic3r --output #{outputfile} #{file}")
-    end
-
-    def print(printer, options={})
+    def slice(config=nil, outfile=nil)
+      outfile ||= @outputfile
+      IO.popen("slic3r --output #{outfile} #{file}")
+      @outputfile = outfile
     end
   end
 end

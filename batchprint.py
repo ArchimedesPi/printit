ad2201365978c6042dff8510b1c2c1af6000907c
ArchimedesPi <archimedespi3141@gmail.com>
2014-06-19 18:51:49 -0500

    Preserve current state

--- batchprint.py
+++ batchprint.py
@@ -1,1 +1,15 @@
 #PYTHON!!!
+
+f = open('bom.bom', 'r')
+parts = []
+
+for line in f:
+  stripit = [x.strip() for x in line.split(',')]
+  parts.append({'file': stripit[0], 'times': int(stripit[1])})
+
+for part in parts:
+  print "Slicing part {file}"
+  
+  print "Printing part {file} {times} times".format(file=part['file'], times=part['times'])
+  for i in range(0, part['times']):
+    print 'print'
